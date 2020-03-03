725fda8c4b56324f6cb806682a439331d37e2ee1
Teo Jao Ming <42195482+jaotheboss@users.noreply.github.com>
2020-03-03 17:56:43 +0800

    Update Data Visualization.py

--- Data Visualization.py
+++ Data Visualization.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan  5 17:38:59 2020
-
-@author: jaoming
-"""
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import os 
